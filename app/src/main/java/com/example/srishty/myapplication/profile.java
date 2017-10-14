d06fd855e02de08920655444ed4c80f3523e4d18
Srishty12rawat <rawatsrishty98@gmail.com>
2017-10-14 23:35:44 +0530

    test

---
+++ app/src/main/java/com/example/srishty/myapplication/profile.java
@@ -0,0 +1,11 @@
+package com.example.srishty.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
---
+++ app/src/main/java/com/example/srishty/myapplication/work_experience.java
@@ -0,0 +1,13 @@
+package com.example.srishty.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class work_experience extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_experience);
+    }
+}
