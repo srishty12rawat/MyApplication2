d1385d300be4091e910696f620ecef636894b275
Srishty12rawat <rawatsrishty98@gmail.com>
2017-10-16 00:22:49 +0530

    test

--- app/src/main/java/com/example/srishty/myapplication/MainActivity.java
+++ app/src/main/java/com/example/srishty/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.srishty.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.education);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                }
+                    });
+          }
     }
-}
